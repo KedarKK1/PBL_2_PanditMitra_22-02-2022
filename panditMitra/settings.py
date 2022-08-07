3e5122329ae8ae9033c04515a2b8fd86a7d7c2d6
KedarKK1 <officialkedark1@gmail.com>
2022-08-08 00:05:16 +0530

    final testing

--- panditMitra/settings.py
+++ panditMitra/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'panditMitra.urls'
+ROOT_URLCONF = 'panditMitra.panditMitra.urls'
 
 TEMPLATES = [
     {
